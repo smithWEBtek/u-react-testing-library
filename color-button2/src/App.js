61d0cd2753b47659f838759bc096a7418fd3ca49
brad@smithwebtek.com <brad@smithwebtek.com>
2022-02-05 21:08:22 -0500

    Add copy of color-button app

---
+++ color-button2/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+
+
+  return (
+    <div>
+      <button
+      ><h1 className='red'>Change to blue</h1></button>
+    </div >
+  );
+}
+
+export default App;
