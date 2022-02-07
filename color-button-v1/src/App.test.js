8f42fda70561539e1bee2893381fe881abc21eec
brad@smithwebtek.com <brad@smithwebtek.com>
2022-02-06 19:47:40 -0500

    Add copy of color-button

---
+++ color-button-v1/src/App.test.js
@@ -0,0 +1,24 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+
+test('button has correct initial color', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' })
+
+  // expect the colorButton to be there
+  expect(colorButton).toBeInTheDocument();
+
+  // expect the colorButton to have background-color of 'red'
+  expect(colorButton).toHaveStyle(`backgroundColor: 'red'`);
+});
+
+test('button text changes to blue when clicked', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', 'Change to bluee');
+
+  expect(colorButton).toHaveStyle(`backgroundColor: 'blue'`);
+});
+
+test('why are changes not showing in vscode?', () => {
+  // asdf
+});
